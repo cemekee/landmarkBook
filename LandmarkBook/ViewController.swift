9ddbfca0c7358ee0c7c16c416fd502ba7988eea3
Cem Eke <cem.eke14@gmail.com>
2021-01-17 22:30:24 +0300

    Initial commit

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -7,13 +7,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    var choosenLandmarkName = ""
+    var choosenLandmarkImage = UIImage()
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    
+        
+        landmarkNames.append("Colosseum")
+        landmarkNames.append("Greatwall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Stonehange")
+        landmarkNames.append("Tacmahal")
+        
+        
+        landmarkImages.append(UIImage(named: "colosseum.jpg")!)
+        landmarkImages.append(UIImage(named: "greatwall.jpg")!)
+        landmarkImages.append(UIImage(named: "kremlin.jpg")!)
+        landmarkImages.append(UIImage(named: "stonehange.jpg")!)
+        landmarkImages.append(UIImage(named: "tacmahal.jpg")!)
+        
+        
+        navigationItem.title = "Landmark Book"
+        
     }
-
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = landmarkNames[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarkNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toImageViewController", sender: nil)
+        choosenLandmarkName = landmarkNames[indexPath.row]
+        choosenLandmarkImage = landmarkImages[indexPath.row]
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toImageViewController" {
+            let destinationVC = segue.destination as! imageViewController
+            destinationVC.selectedLandmarkName = choosenLandmarkName
+            destinationVC.selectedLandmarkImage = choosenLandmarkImage
+        }
+    }
 
 }
 
